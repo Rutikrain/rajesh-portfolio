2cded6838516fdc5de9cd2bfefbf4b18ffb5649c
shrikrushna shinde <shrikrushnashinde9612@gmail.com>
2026-03-20 17:41:52 +0530

    ches

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          RC
+          RR
         </a>
         <a
           href="mailto:rutikrain2002@gmail.com"
