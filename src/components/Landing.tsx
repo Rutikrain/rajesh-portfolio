69388435636e0bf7bc596923698bc020f45749fc
shrikrushna shinde <shrikrushnashinde9612@gmail.com>
2026-03-21 16:19:00 +0530

    tr

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              RUTIK
-              <br />
-              <span>RAIN</span>
+              RUTIK <span>RAIN</span>
             </h1>
           </div>
           <div className="landing-info">
